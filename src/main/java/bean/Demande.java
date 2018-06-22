45cf2253983fe3e4d14b4d61a5dc965beca1804e
Lenovo <Lenovo@DESKTOP-1UC7R7N>
2018-06-23 00:35:23 +0100

    nouvelle version

--- src/main/java/bean/Demande.java
+++ src/main/java/bean/Demande.java
@@ -8,8 +8,6 @@ package bean;
 import java.io.Serializable;
 import java.util.Date;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 import javax.persistence.Temporal;
